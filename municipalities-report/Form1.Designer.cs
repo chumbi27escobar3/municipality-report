08ea32c0fd46c4e3d9c71a62e48dc93ac8611581
Fernanda <FernandaRojas152@hotmail.com>
2021-02-06 14:29:03 -0500

    adding window

---
+++ municipalities-report/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace municipalities_report
+{
+    partial class Statistics
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.file = new System.Windows.Forms.Label();
+            this.data = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.codigoDepartamento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.codigoMunicipio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nombreDepartamento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nombreMunicipio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // file
+            // 
+            this.file.AutoSize = true;
+            this.file.Location = new System.Drawing.Point(148, 26);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(35, 13);
+            this.file.TabIndex = 1;
+            this.file.Text = "label1";
+            // 
+            // data
+            // 
+            this.data.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.codigoDepartamento,
+            this.codigoMunicipio,
+            this.nombreDepartamento,
+            this.nombreMunicipio,
+            this.tipo});
+            this.data.HideSelection = false;
+            this.data.Location = new System.Drawing.Point(28, 50);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(309, 388);
+            this.data.TabIndex = 2;
+            this.data.UseCompatibleStateImageBehavior = false;
+            this.data.View = System.Windows.Forms.View.Details;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(514, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.file);
+            this.Controls.Add(this.button1);
+            this.Name = "Statistics";
+            this.Text = "Statistics";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label file;
+        private System.Windows.Forms.ListView data;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ColumnHeader codigoDepartamento;
+        private System.Windows.Forms.ColumnHeader codigoMunicipio;
+        private System.Windows.Forms.ColumnHeader nombreDepartamento;
+        private System.Windows.Forms.ColumnHeader nombreMunicipio;
+        private System.Windows.Forms.ColumnHeader tipo;
+    }
+}
+
---
+++ municipalities-report/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace municipalities_report
+{
+    public partial class Statistics : Form
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    //file.Text = openFileDialog1.FileName;
+                    file.Text = sr.ReadToEnd();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }   
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
